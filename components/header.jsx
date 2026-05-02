1fa7deb765dc38b8f2196888b210d6428c50494b
Kartik Dhomne <kartikdhomne1997@gmail.com>
2026-05-02 23:44:43 +0530

    Header setup with Clerk Auth

---
+++ components/header.jsx
@@ -0,0 +1,38 @@
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import React from "react";
+
+import { Button } from "./ui/button";
+import Link from "next/link";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <nav className="fixed top-0 inset-x-0 z-50 flex items-center justify-between px-3 sm:px-10 py-3 border-b border-white/7 backdrop-blur-xl">
+      <Link href="/">
+        <Image
+          src="/logo-preply.png"
+          alt="Prept Logo"
+          width={100}
+          height={100}
+          loading="eager"
+        />
+      </Link>
+
+      <div className="flex items-center gap-3">
+        <Show when="signed-out">
+          <SignInButton mode="model">
+            <Button variant="ghost">Sign In</Button>
+          </SignInButton>
+          <SignUpButton mode="model">
+            <Button variant="gold">Get Started</Button>
+          </SignUpButton>
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
